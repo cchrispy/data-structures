62eed603015da512f3f1f775bc7e8e73bd6610cb
cchrispy <cchiang1992@gmail.com>
2016-07-28 10:29:34 -0700

    Completed hashTable data structures

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var obj = {};
+  obj[k] = v;
+  this._storage.set(index, _.extend({}, this._storage.get(index), obj));
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, _.omit(this._storage.get(index), k));
 };
-
+  
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ storage = [{val1: 1, val2: nextvalue} , undefined, blah, 2]
+           undefined[steven]
  */
 
 
