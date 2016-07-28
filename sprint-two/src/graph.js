c8e18d3d468bdc46ffee07cc7f8c6e40f5eb3356
cchrispy <cchiang1992@gmail.com>
2016-07-28 16:21:34 -0700

    Removed Comments and Updated Time Complexities

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -14,9 +14,6 @@ Graph.prototype.addNode = function(key) {
 Graph.prototype.contains = function(key) {
   return this.nodes[key] !== undefined;
 
-  // return _.some(this.nodes, function(eachNode) {
-  //   return eachNode.value === node;
-  // });
 };
 
 // Removes a node from the graph.
@@ -27,21 +24,12 @@ Graph.prototype.removeNode = function(node) {
   });
   delete this.nodes[node];
 
-
-  // var func = this.removeEdge.bind(this);
-  // _.each(this.nodes[this.findNode(node)].refs, function(edgeVal) {
-  //   func(node, edgeVal);
-  // });
-  // this.nodes = _.filter(this.nodes, function(eachNode) {
-  //   return eachNode.value !== node;
-  // });
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   return this.nodes[fromNode][toNode] !== undefined;
 
-  // return this.nodes[this.findNode(fromNode)].refs.includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
@@ -50,9 +38,6 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
   this.nodes[fromNode][toNode] = toNode;
   this.nodes[toNode][fromNode] = fromNode;
 
-  // this.nodes[this.findNode(fromNode)].refs.push(toNode);
-
-  // this.nodes[this.findNode(toNode)].refs.push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
@@ -60,38 +45,26 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
   delete this.nodes[fromNode][toNode];
   delete this.nodes[toNode][fromNode];
 
-  // var i = this.nodes[this.findNode(fromNode)].refs.indexOf(toNode);
-  // this.nodes[this.findNode(fromNode)].refs.splice(i, 1);
-  // var j = this.nodes[this.findNode(toNode)].refs.indexOf(fromNode);
-  // this.nodes[this.findNode(toNode)].refs.splice(j, 1);
+
 };
 
-// Graph.prototype.findNode = function(value) {
-//   return this.nodes.findIndex(function(node) {
-//     return node.value === value;
-//   });
-// };
+
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   _.each(this.nodes, function(node, key) {
     cb(key);
   });
-  // this.nodes.forEach(function(node) {
-  //   cb(node.value);
-  // });
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
   AddNode(): O(1)
-  contains(): O(n)
+  contains(): O(1)
   removeNode(): O(n)
-  findNode(): O(n)
-  If hasEdge and addEdge didn't use findNode, they would have constant time.
-  hasEdge(): O(n)
-  addEdge(): O(n)
+  hasEdge(): O(1)
+  addEdge(): O(1)
   forEachNode(): O(n)
 
  */
