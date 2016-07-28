34ac0cb8b64c09775a5feb6a432829ead67e6d7e
cchrispy <cchiang1992@gmail.com>
2016-07-27 19:57:37 -0700

    Completed Tree Data Structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
 
-  return newTree;
+  newTree.children = [];
+
+  // newTree.children = null;  // fix me
+
+  return _.extend(newTree, treeMethods);
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));
+
+
+  // newTree.children = null;  // fix me
 };
 
 treeMethods.contains = function(target) {
+  return this.find(this, target);
 };
 
+treeMethods.find = function(node, target) {
+  var result = false;
+  node.children.forEach(function(child) {
+    if (child.value === target) {
+      result = true;
+    } else {
+      if (!result) {
+        result = treeMethods.find(child, target) ? true : false;
+      }
+    }
+  });
+  return result;
+};
 
 
 /*
