44feee96f4036fdb98067847d6f993015dd3792c
cchrispy <cchiang1992@gmail.com>
2016-07-28 11:10:42 -0700

    Completed BST data structures
    Bare minimum for all tests passed

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,56 @@
 var BinarySearchTree = function(value) {
-};
+  this.tree = {
+    value: value,
+    right: undefined,
+    left: undefined
+  };
 
+
+
+
+  return _.extend(tree, BinarySearchTreeMethods);
+};
+var BinarySearchTreeMethods = {
+  insert: function(input) {
+    if (this.value < input) {
+      if (this.right === undefined) {
+        this.right = BinarySearchTree(input);
+      } else {
+        this.right.insert(input);
+      }
+    } else if (this.value > input) {
+      if (this.left === undefined) {
+        this.left = BinarySearchTree(input);
+      } else {
+        this.left.insert(input);
+      }
+    }
+  },
+  contains: function(input) {
+    if (input === this.value) {
+      return true;
+    } else if (input > this.value) {
+      if (this.right === undefined) {
+        return false;
+      }
+      return this.right.contains(input);
+    } else if (input < this.value) {
+      if (this.left === undefined) {
+        return false;
+      }
+      return this.left.contains(input);
+    }
+  },
+  depthFirstLog: function(callback) {
+    callback(this.value);
+    if (this.right !== undefined) {
+      this.right.depthFirstLog(callback);
+    }
+    if (this.left !== undefined) {
+      this.left.depthFirstLog(callback);
+    }
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
