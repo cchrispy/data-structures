d7025fabfc14d5b86935b94e8d1b133ca82454ce
cchrispy <cchiang1992@gmail.com>
2016-07-27 19:40:47 -0700

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,44 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  list.iterate = function(node, target) {
+    if (node.next === null) {
+      node.next = target;
+    } else {
+      this.iterate(node.next);
+    }
+  };
+  
+
   list.addToTail = function(value) {
+    this.tail = Node(value);
+    if (this.head === null) {
+      this.head = Node(value);
+    } else {
+      this.iterate(this.head, Node(value));
+    }
   };
 
   list.removeHead = function() {
+    if (this.head !== null) {
+      var result = this.head.value;
+      this.head = this.head.next;
+      return result;
+    }
   };
 
   list.contains = function(target) {
+    return this.find(this.head, target);
+  };
+  
+  list.find = function(node, target) {
+    if (node.value === target) {
+      return true;
+    } else if (node.next === null) {
+      return false;
+    } else {
+      return this.find(node.next, target);
+    }
   };
 
   return list;
@@ -27,3 +58,19 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+// {head: somenode,
+// tail: somenode}
+
+// {head:
+//   next: {value: 
+//     next :{value: value,
+//       next: {
+//         value: value,
+//         next: null
+//       }
+//     }
+//   },
+//  tail: lastNode
+// }
