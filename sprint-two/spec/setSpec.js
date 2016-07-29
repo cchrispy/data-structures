166d88d485cbe201bb14f71682470486291683bb
cchrispy <cchiang1992@gmail.com>
2016-07-29 15:01:29 -0700

    Refactored binary tree and added breadthFirstLog function
    also added test suites. time complexity of breadthFirstLog O(n)

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -16,6 +16,10 @@ describe('set', function() {
     set.add('Danny Glover');
     expect(set.contains('Danny Glover')).to.equal(true);
     expect(set.contains('Susan Sarandon')).to.equal(true);
+    set.add(10);
+    expect(set.contains(10)).to.equal(true);
+    set.add({a: 1});
+    expect(set.contains({a: 1})).to.eql(true);
   });
 
   it('should remove values from a set', function() {
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,7 @@
 var BinarySearchTree = function(value) {
   this.tree = {
+    depth: 0,
+    maxDepth: 0,
     value: value,
     right: undefined,
     left: undefined
@@ -12,15 +14,18 @@ var BinarySearchTree = function(value) {
 };
 var BinarySearchTreeMethods = {
   insert: function(input) {
+    var newChild = BinarySearchTree(input);
+    newChild.depth = this.depth + 1;
+    this.maxDepth = (this.maxDepth > newChild.depth ? this.maxDepth : newChild.depth);
     if (this.value < input) {
       if (this.right === undefined) {
-        this.right = BinarySearchTree(input);
+        this.right = newChild;
       } else {
         this.right.insert(input);
       }
     } else if (this.value > input) {
       if (this.left === undefined) {
-        this.left = BinarySearchTree(input);
+        this.left = newChild;
       } else {
         this.left.insert(input);
       }
@@ -49,7 +54,22 @@ var BinarySearchTreeMethods = {
     if (this.left !== undefined) {
       this.left.depthFirstLog(callback);
     }
-  }
+  },
+  breadthFirstLog: function(callback) {
+    var layers = {};
+    for (var i = 0; i < this.maxDepth; i++) {
+      layers[i] = [];
+    }
+    depthFirstLog(function(node) {
+      layers[node.depth].push(node);
+    });
+    _.each(layers, function(depthArray) {
+      depthArray.each(function(node) {
+        callback(node.value);
+      });
+    });
+  }  
+
 };
 
 /*
@@ -58,3 +78,25 @@ var BinarySearchTreeMethods = {
  contains(): O(log(n))
  depthFirstLog(): O(n)
  */
+/*
+1 2 4 8 16 32
+
+
+
+*/
+// this.left                                this.right
+// this.left.left      this.left.right      this.right.left     this.right.right
+// LLL LLR LRL LRR RLL RLR RRL RRL
+
+//0 1
+//00 01 10 11
+//000 001 010 011 100 101 110 111
+// L( ) R( )
+
+/*
+1: L    R
+2: L(1) R(1)
+3: L(2) R(2)
+4: L(3) R(3)
+*/
+
